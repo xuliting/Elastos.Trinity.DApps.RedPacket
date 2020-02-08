a23de673675a70db1fa8e5a041bc4f3ad8f9e08e
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-02-08 17:41:55 +0800

    Fixed publish signature path

--- store/publish.sh
+++ store/publish.sh
@@ -1,1 +1,1 @@
-trinity-cli publish --news "$1" --did did:elastos:iXcx5AekgYkdWcRPWLwc2ctKrHNBAm545y#primary --didstore ../../../../DAppSignatures/org.elastos.trinity.dapp.redpacket/appdid/
+trinity-cli publish --news "$1" --did did:elastos:iXcx5AekgYkdWcRPWLwc2ctKrHNBAm545y#primary --didstore ../DAppSignatures/org.elastos.trinity.dapp.redpacket/appdid/
