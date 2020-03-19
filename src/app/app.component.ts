0c83e55a0d6c1a370116d7f4b5cfd28495f24f22
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-03-19 23:30:26 +0800

    Fixed back key navigation to exit app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform, NavController } from '@ionic/angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, NavController, IonRouterOutlet } from '@ionic/angular';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { PacketService } from './services/packet.service';
@@ -9,14 +9,31 @@ import { PacketService } from './services/packet.service';
   templateUrl: 'app.html'
 })
 export class MyApp {
-
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, packetService: PacketService, navController: NavController) {
+  @ViewChild(IonRouterOutlet, {static: true}) routerOutlet: IonRouterOutlet;
+  
+  constructor(private platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, packetService: PacketService, navController: NavController) {
     platform.ready().then(() => {
       packetService.init();
       statusBar.styleDefault();
       splashScreen.hide();
-      
+
+      this.setupBackKeyNavigation();
+
       navController.navigateRoot("/home");
+    });
+  }
+
+  /**
+   * Listen to back key events. If the default router can go back, just go back.
+   * Otherwise, exit the application.
+   */
+  setupBackKeyNavigation() {
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      if (this.routerOutlet && this.routerOutlet.canGoBack()) {
+        this.routerOutlet.pop();
+      } else {
+        navigator['app'].exitApp();
+      }
     });
   }
 }
